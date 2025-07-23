fedfd30b8d8829a4ec514c6d6ced5943d3a3f712
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2025-07-23 23:34:24 +0700

    feat(auth): initialize the login feature

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt'
 import { prisma } from '../../..'
 import { registerDto } from '../dto/register.dto'
 import { PrismaClientKnownRequestError } from '../../../generated/prisma/runtime/library'
+import { loginDto } from '../dto/login.dto'
 
 const router = express.Router()
 
@@ -69,6 +70,63 @@ router.post('/registration', checkSchema(registerDto), async (req: Request, res:
                 data: null,
             })
         }
+    }
+})
+
+router.post('/login', checkSchema(loginDto), async (req: Request, res: Response) => {
+    try {
+        const validatorResult = validationResult(req)
+
+        if (validatorResult.isEmpty()) {
+            const user = await prisma.users.findFirst({
+                where: {
+                    email: req.body.email,
+                }
+            })
+
+            if (user) {
+                const isMatch = await bcrypt.compare(req.body.password, user.password)
+
+                if (isMatch) {
+                    return res.status(200).json({
+                        status: 0,
+                        message: 'Login sukses',
+                        data: {
+                            token: null,
+                        }
+                    })
+                } else {
+                    return res.status(400).json({
+                        status: 103,
+                        message: 'Username atau password salah',
+                        data: null,
+                    })
+                }
+            } else {
+                return res.status(400).json({
+                    status: 103,
+                    message: 'Username atau password salah',
+                    data: null,
+                })
+            }
+        } else {
+            const errors = validatorResult.array()
+            const firstError = errors.at(0)
+
+            return res.status(400).json({
+                status: 102,
+                message: firstError?.msg ?? '',
+                data: null,
+            })
+        }
+    } catch (err) {
+        console.error('[AUTH]: Terjadi kesalahan ketika login pengguna: ', err)
+
+        return res.status(500).json({
+            status: 500,
+            message: 'Internal server error',
+            data: null,
+        })
     }
 })
 
