4eb993e2e184e013ae7c93b3f3a255656b017da0
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2025-07-24 09:41:57 +0700

    feat(cloudinary): initialize the cloudinary helper

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,15 +7,31 @@ interface IConfig {
     NODE_ENV: string
     JWT_SECRET: string
     JWT_EXP_TIME: number
+    CLOUDINARY_API_KEY: string
+    CLOUDINARY_API_SECRET: string
+    CLOUDINARY_CLOUD_NAME: string
 }
 
 const APP_PORT = process.env.APP_PORT ? Number(process.env.APP_PORT) : 3000
 const NODE_ENV = process.env.NODE_ENV ?? 'development'
 const JWT_SECRET = process.env.JWT_SECRET
 const JWT_EXP_TIME = process.env.JWT_EXP_TIME ? Number(process.env.JWT_EXP_TIME) : 43200
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET
+const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME
 
 if (!JWT_SECRET) {
     throw new Error('JWT_SECRET tidak ditemukan!')
+}
+
+if (!CLOUDINARY_API_KEY) {
+    throw new Error('CLOUDINARY_API_KEY tidak ditemukan!')
+}
+if (!CLOUDINARY_API_SECRET) {
+    throw new Error('CLOUDINARY_API_SECRET tidak ditemukan!')
+}
+if (!CLOUDINARY_CLOUD_NAME) {
+    throw new Error('CLOUDINARY_CLOUD_NAME tidak ditemukan!')
 }
 
 const config: IConfig = {
@@ -23,6 +39,9 @@ const config: IConfig = {
     NODE_ENV,
     JWT_SECRET,
     JWT_EXP_TIME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET,
+    CLOUDINARY_CLOUD_NAME,
 }
 
 export default config
